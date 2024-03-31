022c352d48cd549a8c1c317acadb8647f7e16cd4
DvitDavid <31122094+DvitDavid@users.noreply.github.com>
2024-03-31 11:06:17 +0200

    add progressbar
    Include a "progressbar" when downloading and converting the file.

--- youtube.py
+++ youtube.py
@@ -1,8 +1,9 @@
 import flet 
 import os
 import subprocess
+import time
 
-from flet import AlertDialog, Container, ElevatedButton, Page, ProgressRing, Row, Text, TextField
+from flet import AlertDialog, Container, ElevatedButton, Page, ProgressBar, Row, Text, TextField
 from pytube import YouTube
 
 
@@ -12,11 +13,13 @@ def main(page:Page):
     page.title = "Descarregar de Youtube"
     page.window_width=730
     page.window_height=260
+    page.splash = ProgressBar(visible=False)
     lbl_titol = Text("Descarregar video o audio de Youtube", size=30)
     txt_url = TextField(label='URL', autofocus=True)
     bt_enviar_video = ElevatedButton("Descarregar Video")
     bt_enviar_audio = ElevatedButton("Descarregar Audio")
     dlg_missatge = AlertDialog(title=Text("Descàrrega exitosa."),  on_dismiss=lambda _: print("test"))
+    
          
 
     def bt_click(event):
@@ -35,18 +38,21 @@ def main(page:Page):
         carpeta_actual = os.getcwd()
         #print(carpeta_actual)
         yt = YouTube(txt_url.value)
+        page.splash = ProgressBar(visible=True)
         audio= yt.streams.filter(only_audio=True, subtype='webm', abr='160kbps').first()
 
         final_audio = audio.download(output_path = carpeta_actual)
+        page.update()
         
         base, ext = os.path.splitext(final_audio) 
         nou_audio = base + '.mp3'
-        os.rename(final_audio, nou_audio)      
+        os.rename(final_audio, nou_audio)
+        time.sleep(0.5)      
         
         page.dialog = dlg_missatge
         dlg_missatge.content=Text(f"{carpeta_actual}")
         dlg_missatge.open = True
-        
+        page.splash = ProgressBar(visible=False)        
         page.update()   
 
     
